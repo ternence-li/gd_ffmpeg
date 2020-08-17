03fc0d983db8fbcbf83b4dd7fa14cfbaa8d2fcd6
Pawel Piecuch <pawel.piecuch@roche.com>
2020-08-17 14:52:13 +0200

    Godot interface with updated FFmpeg library

---
+++ src/gd_videodecoder.h
@@ -0,0 +1,9 @@
+#ifndef GD_FFMPEG_VIDEODECODER_H
+#define GD_FFMPEG_VIDEODECODER_H
+
+extern "C" {
+	void gdffmpeg_init(godot_gdnative_core_api_struct *api_struc);
+	void gdffmpeg_terminate(godot_gdnative_core_api_struct *api_struc);
+} // extern "C"
+
+#endif /* GD_FFMPEG_VIDEODECODER_H */
---
+++ src/register_types.cpp
@@ -0,0 +1,15 @@
+#include "register_types.h"
+
+#include "core/class_db.h"
+#include "gdnative/gdnative.h"
+#include "gd_videodecoder.h"
+
+void register_gd_ffmpeg_types() {
+
+	// Defined in gdnative_api_struct.gen.cpp
+	extern const godot_gdnative_core_api_struct api_struct;
+
+}
+
+void unregister_gd_ffmpeg_types() {
+}
